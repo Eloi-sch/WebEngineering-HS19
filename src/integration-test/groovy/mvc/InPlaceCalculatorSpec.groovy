91f0056e0f18649e5773719c40bce02ae087beb6
Christian Ribeaud <christian.ribeaud@fhnw.ch>
2019-10-26 12:28:07 +0200

    Add new branch 'integration-test'

--- src/integration-test/groovy/mvc/InPlaceCalculatorSpec.groovy
+++ src/integration-test/groovy/mvc/InPlaceCalculatorSpec.groovy
@@ -16,8 +16,8 @@ class InPlaceCalculatorSpec extends GebSpec {
         title == "In-Place Calculator"
 
         when: "set valid input"
-        $("form").en = 5.0
-        $("form").exam = 6.0
+        $("form").en = 5
+        $("form").exam = 6
         $("input", type: "submit").click()
 
         then: "Result is displayed with proper rounding up"
